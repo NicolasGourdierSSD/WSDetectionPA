76235cd93f551dc46e41a235d47600eaf29725b6
Nicolas Gourdier <dev2@solutionssantedigitale.com>
2021-05-26 17:04:32 +0200

    qmolsdfjqmsldkjqmlsdfj

--- startup.sh
+++ startup.sh
@@ -1,7 +1,8 @@
 apt-get update
 apt install libopencv-dev python3-opencv
 apt-get install libgl1-mesa-glx
-apt-get install ffmpeg libsm6 libxext6
+apt-get install -y libgl1-mesa-dev
+apt-get install ffmpeg libsm6 libxext6  -y
 apt-get install libglib2.0-0
 echo "---------- démarrage ----------"
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
