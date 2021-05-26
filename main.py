3ae101b2681a4e10c5201121d45459feb3a4c646
Nicolas Gourdier <dev2@solutionssantedigitale.com>
2021-05-26 13:52:31 +0200

    Added Post + np + file

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-#from typing import Optional
+from typing import Optional
 from fastapi import FastAPI
-#from fastapi import File
-#from fastapi import UploadFile
-#from pydantic import BaseModel
+from fastapi import File
+from fastapi import UploadFile
+from pydantic import BaseModel
 #import cv2 # opencv
-#import numpy as np
+import numpy as np
 
-#from PIL import Image
+from PIL import Image
 
 app = FastAPI()
 
@@ -14,9 +14,9 @@ app = FastAPI()
 async def read_root():
     return {"message": "Welcome from the API"}
 
-# @app.post("/get")
-# async def get_pa(file: UploadFile = File(...)): # get pression artérielle
-#     image = np.array(Image.open(file.file))
-#     width, height, _ = image.shape
+@app.post("/get")
+async def get_pa(file: UploadFile = File(...)): # get pression artérielle
+    image = np.array(Image.open(file.file))
+    width, height, _ = image.shape
 #     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-#     return {"Width":width,"Height":height}
+    return {"Width":width,"Height":height}
