8c3a742e91307c1d1778337c8a4a2b7317554113
Nicolas Gourdier <dev2@solutionssantedigitale.com>
2021-05-26 14:46:13 +0200

    an other try with apt-get install libgl1-mesa-glx

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 apt-get update
 apt install libopencv-dev python3-opencv
+apt-get install libgl1-mesa-glx
 #apt-get install ffmpeg libsm6 libxext6  -y
 gunicorn -w 4 -k uvicorn.workers.UvicornWorker main:app
